6cb7706b78cc03441b495d56294f90e9a335c922
teolamm <156580714+teolamm@users.noreply.github.com>
2024-01-14 18:49:49 +0200

    Add files via upload

--- db_queries.py
+++ db_queries.py
@@ -11,41 +11,39 @@ class Queries:
 
     def queries(self):
 
+        print("\nlist of all the drivers living in Athens:")
         ath = "Athens"
-        self.cursor.execute("select όνομα, επίθετο, τόπος from ΙΔΙΟΚΤΗΤΗΣ where τόπος = '" + str(ath) + "';")
+        self.cursor.execute("select όνομα, επίθετο from ΙΔΙΟΚΤΗΤΗΣ where τόπος = '" + str(ath) + "';")
         drivers = self.cursor.fetchall()
         for driver in drivers:
-            print(driver)
+            print(driver[0], driver[1])
 
-        print("\n")
-
+        print("\nlist with all cities having an ev recharge station:")
         self.cursor.execute('''select distinct τόπος from ΣΤΑΘΜΟΣ order by τόπος''')
         locs = self.cursor.fetchall()
         for loc in locs:
             print(loc[0])
 
-        print("\n")
-
+        print("\nlist of every station and its average review: ")
         self.cursor.execute('''select avg(αστέρια), id_σταθμού, τόπος, διεύθυνση
                                 from ΚΡΙΤΙΚΗ natural join ΣΤΑΘΜΟΣ
-                                group by id_σταθμού''')
+                                group by id_σταθμού
+                                having avg(αστέρια)>0''')
         stars = self.cursor.fetchall()
         for st in stars:
-            print(st)
+            print("station id:", st[1], "at: ", st[2] + ",", st[3], "average review: {:.2f} stars".format(st[0]))
 
-        print("\n")
-
+        print("\nlist of all stations with dc chargers: ")
         self.cursor.execute('''select distinct id_σταθμού, τόπος, διεύθυνση
                                 from ΣΤΑΘΜΟΣ natural join ΘΕΣΗ
                                 where τύπος_φορτιστή = "dc"
                             ''')
         fast = self.cursor.fetchall()
         for f in fast:
-            print(f)
+            print("station id:", f[0], "at: ", f[1] + ",", f[2])
 
-        print("\n")
-
-        self.cursor.execute('''select  id_οδηγού, sum(ενέργεια_kWh)
+        print("\nlist of every driver and total energy of all their charges: ")
+        self.cursor.execute('''select  id_οδηγού, πινακίδες, sum(ενέργεια_kWh)
                                 from ΦΟΡΤΙΣΗ join ΑΥΤΟΚΙΝΗΤΟ on πινακίδες = πινακίδες_αυτοκινήτου
                                 group by id_οδηγού
                                 having sum(ενέργεια_kWh) > 1
@@ -53,28 +51,24 @@ class Queries:
                                 ''')
         energy = self.cursor.fetchall()
         for e in energy:
-            print(e)
+            print("driver's id:", e[0], "- car's plates:", e[1], "- total energy consumption:", e[2])
 
-        print("\n")
-
+        print("\nlist of all the Teslas and their construction years: ")
         self.cursor.execute('''select μοντέλο, έτος_κατασκευής
                                 from ΑΥΤΟΚΙΝΗΤΟ
                                 where μάρκα = 'Tesla'
                                 ''')
         teslas = self.cursor.fetchall()
         for tesla in teslas:
-            print(tesla)
-
-        print("\n")
+            print(tesla[0], "-", tesla[1])
 
         self.cursor.execute('''select max(αριθμός_ραντεβού), max(αριθμός_φόρτισης),  100 * cast(max(αριθμός_ραντεβού) as decimal(2,2)) / cast(max(αριθμός_φόρτισης) as decimal(2,2))
                                 from ΡΑΝΤΕΒΟΥ natural join ΦΟΡΤΙΣΗ
                                 ''')
         stat = self.cursor.fetchone()
-        print("number of appointments: " + str(stat[0]) + "\nnumber of charges: " + str(stat[1]) + "\npercentage of charges happening after an appointment: " + str(stat[2]) + "%")
+        print("\nnumber of appointments: " + str(stat[0]) + "\nnumber of charges: " + str(stat[1]) + "\npercentage of charges happening after an appointment: " + str(stat[2]) + "%")
 
-        print("\n")
-
+        print("\nlist of available chargers per station: ")
         self.cursor.execute('''select id_σταθμού, count(*)
                                 from ΣΤΑΘΜΟΣ natural join ΘΕΣΗ
                                 where κατειλημμένη = 0
@@ -84,6 +78,6 @@ class Queries:
                             ''')
         free_chargers = self.cursor.fetchall()
         for ch in free_chargers:
-            print(ch)
+            print("station id:",ch[0], "- free spots: ", ch[1])
 
 q1 = Queries()
